668ad4e79fad3c98c52441ddcc851b6a91a26992
nicoud1027 <nicoud1027@gmail.com>
2026-03-01 13:17:52 -0500

    Añadido 2 algoritmos

---
+++ burbujas.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+        int numeros[] = {3,2,1,4,5};
+        int i,j,aux;
+        int pasos=0;
+        int pasosciclo=0;
+	for (i=0;i<5;i++){
+//pasos+=2;
+                for(j=0;j<5;j++){
+       			pasos+=2;
+                        if(numeros[j] > numeros[j+1]){
+                                aux = numeros[j];
+                                numeros[j] = numeros[j+1];
+                                numeros[j+1] = aux;
+                                pasos+=3;
+                        }
+                pasosciclo++;
+    		}            
+        pasos++;
+        }
+
+        cout<<"orden ascendente: ";
+        for(i=0;i<5;i++){
+         cout<<numeros[i]<<" ";
+        }
+        cout<<"\norden descendente: ";
+        for(j=4;j>=0;j--){
+         cout<<numeros[j]<<" ";
+        }
+        cout<<"\npasos: "<< pasos <<endl;
+        cout<<"pasos ciclo: " <<pasosciclo<<endl;
+	
+return 0;
+
+}
---
+++ quick.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int partition(int num[], int in, int f);
+int pasos = 0;
+int pasosciclo = 0;
+
+void quickSort(int num[], int in, int f) {
+	pasos++;
+    if (in < f) {
+	pasos++;
+        int aux = partition(num, in, f); //Creacion de Particion
+	pasos++;
+        quickSort(num, in, aux - 1); //Por parte izq
+	pasos++;
+        quickSort(num, aux + 1, f); //Por parte derecha
+	pasos+=2;
+    }
+	
+}
+
+int partition(int num[], int in, int f) {
+    pasos++;	
+    int aux = num[f]; //Pivote num final
+    pasos++;
+    int i = in - 1;
+    pasos++;
+    for (int j = in; j < f; j++) { //For comparativo
+	pasosciclo++;
+        if (num[j] < aux) {
+	    pasos++;
+            i++;
+	    int j = rand () % (i+1);
+            pasos++;
+            swap(num[i], num[j]); // Swap
+	pasos++;
+        }
+	pasos++; 
+    }
+    swap(num[i + 1], num[f]); //Swap final pivote
+    pasos++;
+    return i + 1;
+}
+
+
+int main() {
+    int numeros[100];
+    int n = 100;
+
+    quickSort(numeros, 0, n - 1);
+
+    cout << "pasos: "<< pasos << " " <<endl;
+    cout << "ciclo: "<< pasosciclo << " ";
+    return 0;
+}
